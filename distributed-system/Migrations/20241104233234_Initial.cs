0dabd97f1900196b4d7e78c00b7a415a0a139df5
Leonardo Spilere <leonardo.spilere@deps.com.br>
2024-11-04 20:34:19 -0300

    config final

---
+++ distributed-system/Migrations/20241104233234_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace distributedsystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    ProductType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
--- distributed-system/Startup.cs
+++ distributed-system/Startup.cs
@@ -1,10 +1,14 @@
-﻿using distributed_system.Services;
+﻿using distributed_system.Context;
+using distributed_system.Services;
+using Microsoft.EntityFrameworkCore;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddGrpc();
+        services.AddDbContext<InventoryContext>(options =>
+            options.UseNpgsql(Program.Configuration.GetConnectionString("PostgresConnection")));
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
